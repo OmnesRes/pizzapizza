256b491d0aeae35128c44f147ba19d1b73affb08
Jordan Anaya <jordananaya@gmail.com>
2017-01-15 03:46:06 -0500

    R update

--- R/Wansink-reanalyses.R
+++ R/Wansink-reanalyses.R
@@ -1,108 +1,160 @@
 library(rpsychi)
 
-#Example code - compare with real ANOVA from raw data in SPSS
-m.mat <- matrix(c(0.6006575, 0.6041635, 0.4552795, 0.3481225, 0.6484595, 0.579725), ncol=2)
-sd.mat <- matrix(c(0.20422236, 0.37179451, 0.29778293, 0.23264826, 0.2919474, 0.27165736), ncol=2)
-n.mat <- matrix(c(20, 20, 20, 20, 20, 20), ncol=2)
-au <- ind.twoway.second(unbiased=FALSE, m = m.mat, sd = sd.mat, n = n.mat)
-ab <- ind.twoway.second(m = m.mat, sd = sd.mat, n = n.mat)
-
-m.vec <- c(4.77, 6.24)
-sd.vec <- c(1.93, 2.65)
-n.vec <- c(46, 39)
-t <- ind.oneway.second(unbiased=FALSE, m = m.vec, sd = sd.vec, n = n.vec)
-
-f_range <- function (m.p, sd.p, n.p, title=FALSE, u.p=TRUE, show.t=FALSE, labels=c()) {
-  if (class(m.p) == "matrix") {
+# Function to display the possible ranges of the F or t statistic from a one- or two-way ANOVA.
+# Assumes means and SDs have been rounded to two decimal place.
+f_range <- function (m.p, sd.p, n.p, title=FALSE, u.p=TRUE, show.t=FALSE, dp=2, labels=c()) {
+  m.ok <- m.p
+  if (class(m.ok) == "matrix") {
     func <- ind.twoway.second
-    ways <- 2
+    useF <- c(3, 2, 4)
+    default_labels <- c("col F", "row F", "inter F")
   }
   else {
-    m.p <- matrix(m.p)
+    m.ok <- matrix(m.p)
     func <- ind.oneway.second
-    ways <- 1
-  }
-
-# Correct for rounding in reported numbers by adding possible discrepancy (assume 2 DPs).
-# For maximum F estimate, we subtract .005 from all SDs; for minimum F estimate, we add .005.
-# For maximum F estimate, we have two methods to adjust the means:
-# 1. Add .005 to the highest mean and subtract .005 from the lowest mean (assumes means are positive!)
-# 2. Add .005 to all means above the midpoint and subtract .005 from all means below the midpoint
-# Then we choose whichever gives the widest range. (For 2-condition items, they will be identical.)
-# For minimum F estimate, we perform the opposite operations to the above.
-  m.hi1 <- m.p
-  m.lo1 <- m.p
-  m.hi2 <- m.p
-  m.lo2 <- m.p
-  for (j in 1:ncol(m.p)) {
-    w.min <- which.min(m.p[,j])
-    w.max <- which.max(m.p[,j])
-    m.hi1[w.min, j] <- m.p[w.min, j] - 0.005
-    m.hi1[w.max, j] <- m.p[w.max, j] + 0.005
-    m.lo1[w.min, j] <- m.p[w.min, j] + 0.005
-    m.lo1[w.max, j] <- m.p[w.max, j] - 0.005
-
-    w.mean <- mean(m.p[,j])
-    for (i in 1:length(m.p[,j])) {
-      cur <- m.p[i, j]
-      delta <- if (cur > w.mean) 0.005 else -0.005
-      m.hi2[i, j] <- cur + delta
-      m.lo2[i, j] <- cur - delta
-    }
-  }
-
-  sd.hi <- sd.p - 0.005
-  sd.lo <- sd.p + 0.005
-
-  if (ways == 1) {
     useF <- 1
     default_labels <- c("F")
     if (show.t) {
       default_labels <- c("t")
     }
   }
-  else {
-    useF <- c(3, 2, 4)
-    default_labels <- c("col F", "row F", "inter F")
-  }
 
   if (length(labels) == 0) {
     labels <- default_labels
   }
 
-  if (title != FALSE) {
-    cat(title, " ")
+# Calculate the nominal test statistic(s) (i.e., assuming no rounding error)
+  f.nom <- func(m=m.ok, sd=sd.p, n=n.p, unbiased=u.p)$anova.table$F
+
+# We correct for rounding in reported numbers by allowing for the maximum possible rounding error.
+# For the maximum F estimate, we subtract .005 from all SDs; for minimum F estimate, we add .005.
+# We then add or subtract .005 to every mean, in all possible permutations.
+# (".005" is an example, based on 2 decimal places of precision.)
+  delta <- (0.1 ^ dp) / 2    #typically 0.005
+  sd.hi <- sd.p - delta
+  sd.lo <- sd.p + delta
+
+# Initialise maximum and minimum F statistics to unlikely values.
+  f.hi <- rep(-1, length(useF))
+  f.lo <- rep(999999, length(useF))
+  f.hi <- f.nom
+  f.lo <- f.nom
+
+# Generate every possible combination of +/- maximum rounding error to add to each mean.
+  l <- length(m.ok)
+  rawcomb <- combn(rep(c(-delta, delta), l), l)
+  comb <- rawcomb[,!duplicated(t(rawcomb))]
+
+# Generate every possible set of test statistics within the bounds of rounding error,
+#  and retain the largest and smallest.
+  for (i in 1:ncol(comb)) {
+    m.adj <- m.ok + comb[,i]
+    f.hi <- pmax(f.hi, func(m=m.adj, sd=sd.hi, n=n.p, unbiased=u.p)$anova.table$F)
+    f.lo <- pmin(f.lo, func(m=m.adj, sd=sd.lo, n=n.p, unbiased=u.p)$anova.table$F)
   }
 
-  f_nom <- func(m=m.p, sd=sd.p, n=n.p, unbiased=u.p)$anova.table$F
-  f_hi1 <- func(m=m.hi1, sd=sd.hi, n=n.p, unbiased=u.p)$anova.table$F
-  f_lo1 <- func(m=m.lo1, sd=sd.lo, n=n.p, unbiased=u.p)$anova.table$F
-  f_hi2 <- func(m=m.hi2, sd=sd.hi, n=n.p, unbiased=u.p)$anova.table$F
-  f_lo2 <- func(m=m.lo2, sd=sd.lo, n=n.p, unbiased=u.p)$anova.table$F
-
-  f_hi <- pmax(f_hi1, f_hi2)
-  f_lo <- pmin(f_lo1, f_lo2)
-  
   if (show.t) {
-    f_nom <- sqrt(f_nom)
-    f_hi <-  sqrt(f_hi)
-    f_lo <-  sqrt(f_lo)
+    f.nom <- sqrt(f.nom)
+    f.hi <-  sqrt(f.hi)
+    f.lo <-  sqrt(f.lo)
   }
 
-  sp <- ""
+  if (title != FALSE) {
+    cat(title)
+  }
+  
+  sp <- " "
+  dpf <- paste("%.", dp, "f", sep="")
   for (i in 1:length(useF)) {
-    cat(sp)
     j <- useF[i]
-    cat(labels[i], ": ", sprintf("%.3f", f_nom[j]),
-                   " (min=", sprintf("%.3f", f_lo[j]),
-                   ", max=", sprintf("%.3f", f_hi[j]), ")",
+    cat(sp, labels[i], ": ", sprintf(dpf, f.nom[j]),
+                   " (min=", sprintf(dpf, f.lo[j]),
+                   ", max=", sprintf(dpf, f.hi[j]), ")",
         sep="")
     sp <- "  "
   }
+
   cat("\n", sep="")
 }
 
-cat("Eating Heavily - Table 1\n")
+cat("Article 1 - Lower Buffet Prices - Table 1\n")
+
+n.lbp.t1 <- c(62, 60)
+
+m.lbp.t1.l1 <- c(44.16, 46.08)
+sd.lbp.t1.l1 <- c(18.99, 14.46)
+f_range(m = m.lbp.t1.l1, sd = sd.lbp.t1.l1, n = n.lbp.t1, title="Age")
+
+m.lbp.t1.l3 <- c(68.52, 67.91)
+sd.lbp.t1.l3 <- c(3.95, 3.93)
+f_range(m = m.lbp.t1.l3, sd = sd.lbp.t1.l3, n = n.lbp.t1, title="Height")
+
+m.lbp.t1.l4 <- c(180.84, 182.31)
+sd.lbp.t1.l4 <- c(48.37, 48.41)
+f_range(m = m.lbp.t1.l4, sd = sd.lbp.t1.l4, n = n.lbp.t1, title="Weight")
+
+m.lbp.t1.l5 <- c(3.00, 3.28)
+sd.lbp.t1.l5 <- c(1.55, 1.29)
+f_range(m = m.lbp.t1.l5, sd = sd.lbp.t1.l5, n = n.lbp.t1, title="Group size")
+
+# Next line gives an F too small for rpsychi to calculate
+#m.lbp.t1.l6 <- c(6.62, 6.64)
+#sd.lbp.t1.l6 <- c(1.85, 2.06)
+#f_range(m = m.lbp.t1.l6, sd = sd.lbp.t1.l6, n = n.lbp.t1, title="Hungry then")
+
+m.lbp.t1.l7 <- c(1.88, 1.85)
+sd.lbp.t1.l7 <- c(1.34, 1.75)
+f_range(m = m.lbp.t1.l7, sd = sd.lbp.t1.l7, n = n.lbp.t1, title="Hungry now")
+
+cat("\n")
+cat("Article 1 - Lower Buffet Prices - Table 2\n")
+
+n.lbp.t2.1 <- c(62, 60)   #Lines 1-4
+n.lbp.t2.2 <- c(41, 26)   #Lines 5-7
+n.lbp.t2.3 <- c(47, 38)   #Lines 5-7
+
+m.lbp.t2.l1 <- c(6.89, 7.44)
+sd.lbp.t2.l1 <- c(1.39, 1.60)
+f_range(m = m.lbp.t2.l1, sd = sd.lbp.t2.l1, n = n.lbp.t2.1, title="Overall taste")
+
+m.lbp.t2.l2 <- c(7.08, 7.45)
+sd.lbp.t2.l2 <- c(1.30, 1.60)
+f_range(m = m.lbp.t2.l2, sd = sd.lbp.t2.l2, n = n.lbp.t2.1, title="Piece 1, taste")
+
+m.lbp.t2.l3 <- c(7.08, 7.34)
+sd.lbp.t2.l3 <- c(1.37, 1.70)
+f_range(m = m.lbp.t2.l3, sd = sd.lbp.t2.l3, n = n.lbp.t2.1, title="Piece 1, satisfying")
+
+m.lbp.t2.l4 <- c(7.05, 7.47)
+sd.lbp.t2.l4 <- c(1.40, 1.55)
+f_range(m = m.lbp.t2.l4, sd = sd.lbp.t2.l4, n = n.lbp.t2.1, title="Piece 1, enjoyable")
+
+m.lbp.t2.l5 <- c(6.68, 7.97)
+sd.lbp.t2.l5 <- c(1.49, 1.21)
+f_range(m = m.lbp.t2.l5, sd = sd.lbp.t2.l5, n = n.lbp.t2.2, title="Piece 2, taste")
+
+m.lbp.t2.l6 <- c(6.68, 7.97)
+sd.lbp.t2.l6 <- c(1.49, 1.21)
+f_range(m = m.lbp.t2.l6, sd = sd.lbp.t2.l6, n = n.lbp.t2.2, title="Piece 2, satisfying")
+
+m.lbp.t2.l7 <- c(6.64, 7.81)
+sd.lbp.t2.l7 <- c(1.48, 1.22)
+f_range(m = m.lbp.t2.l7, sd = sd.lbp.t2.l7, n = n.lbp.t2.2, title="Piece 2, enjoyable")
+
+m.lbp.t2.l8 <- c(6.15, 7.58)
+sd.lbp.t2.l8 <- c(1.89, 1.39)
+f_range(m = m.lbp.t2.l8, sd = sd.lbp.t2.l8, n = n.lbp.t2.3, title="Piece 3, taste")
+
+m.lbp.t2.l9 <- c(6.16, 7.41)
+sd.lbp.t2.l9 <- c(1.87, 1.55)
+f_range(m = m.lbp.t2.l9, sd = sd.lbp.t2.l9, n = n.lbp.t2.3, title="Piece 3, satisfying")
+
+m.lbp.t2.l10 <- c(5.98, 7.45)
+sd.lbp.t2.l10 <- c(1.86, 1.52)
+f_range(m = m.lbp.t2.l10, sd = sd.lbp.t2.l10, n = n.lbp.t2.3, title="Piece 3, enjoyable")
+
+cat("\n")
+cat("Article 3 - Eating Heavily - Table 1\n")
 
 n.eh.t1.men <- c(40, 20)
 n.eh.t1.women <- c(35, 10)
@@ -140,7 +192,7 @@ sd.eh.t1.l4.women <- c(3.53, 3.68)
 f_range(m = m.eh.t1.l4.women, sd = sd.eh.t1.l4.women, n = n.eh.t1.women, title="Line 4, women", show.t=TRUE)
 
 cat("\n")
-cat("Eating Heavily - Table 2\n")
+cat("Article 3 - Eating Heavily - Table 2\n")
 
 lab.eh.t2 <- c("gender", "group", "gender x group")
 n.eh.t2 <- matrix(c(40, 20, 35, 10), ncol=2)
@@ -170,7 +222,7 @@ sd.eh.t2.l6 <- matrix(c(1.54, 1.75, 1.71, 2.12), ncol=2)
 f_range(m = m.eh.t2.l6, sd = sd.eh.t2.l6, n = n.eh.t2, title="Line 6", labels=lab.eh.t2)
 
 cat("\n")
-cat("Eating Heavily - Table 3\n")
+cat("Article 3 - Eating Heavily - Table 3\n")
 
 n.eh.t3 <- c(20, 21, 19)
 
@@ -199,7 +251,24 @@ sd.eh.t3.l6 <- c(1.75, 1.77, 1.24)
 f_range(m = m.eh.t3.l6, sd = sd.eh.t3.l6, n = n.eh.t3, title="Line 6")
 
 cat("\n")
-cat("Low Prices and High Regret - Table 2\n")
+cat("Article 4 - Low Prices and High Regret - Table 1\n")
+
+n.lp.t1 <- c(43, 52)
+
+m.lp.t1.l1 <- c(43.67, 44.55)
+sd.lp.t1.l1 <- c(18.50, 14.30)
+f_range(m = m.lp.t1.l1, sd = sd.lp.t1.l1, n = n.lp.t1, title="Age", show.t=TRUE)
+
+m.lp.t1.l2 <- c(68.65, 66.51)
+sd.lp.t1.l2 <- c(3.67, 9.44)
+f_range(m = m.lp.t1.l2, sd = sd.lp.t1.l2, n = n.lp.t1, title="Height", show.t=TRUE)
+
+m.lp.t1.l3 <- c(184.83, 178.38)
+sd.lp.t1.l3 <- c(63.70, 45.71)
+f_range(m = m.lp.t1.l3, sd = sd.lp.t1.l3, n = n.lp.t1, title="Weight", show.t=TRUE)
+
+cat("\n")
+cat("Article 4 - Low Prices and High Regret - Table 2\n")
 
 lab.lp.t2 <- c("price", "pieces", "price x pieces")
 n.lp.t2 <- matrix(c(18, 18, 7, 17, 19, 10), ncol=2)
@@ -225,7 +294,7 @@ sd.lp.t2.l5 <- matrix(c(2.20, 2.44, 2.22, 1.45, 1.77, 2.81), ncol=2)
 f_range(m = m.lp.t2.l5, sd = sd.lp.t2.l5, n = n.lp.t2, title="Line 5", labels=lab.lp.t2)
 
 cat("\n")
-cat("Low Prices and High Regret - Table 3\n")
+cat("Article 4 - Low Prices and High Regret - Table 3\n")
 
 n.lp.t3.1p <- c(18, 19)
 n.lp.t3.2p <- c(18, 21)
@@ -291,77 +360,3 @@ m.lp.t3.l5.3p <- c(4.57, 4.00)
 sd.lp.t3.l5.3p <- c(2.23, 3.02)
 f_range(m = m.lp.t3.l5.3p, sd = sd.lp.t3.l5.3p, n = n.lp.t3.3p, title="Line 5, 3 pieces")
 
-cat("\n")
-cat("Lower Buffet Prices - Table 1\n")
-
-n.lbp.t1 <- c(62, 60)
-
-m.lbp.t1.l1 <- c(44.16, 46.08)
-sd.lbp.t1.l1 <- c(18.99, 14.46)
-f_range(m = m.lbp.t1.l1, sd = sd.lbp.t1.l1, n = n.lbp.t1, title="Age")
-
-m.lbp.t1.l3 <- c(68.52, 67.91)
-sd.lbp.t1.l3 <- c(3.95, 3.93)
-f_range(m = m.lbp.t1.l3, sd = sd.lbp.t1.l3, n = n.lbp.t1, title="Height")
-
-m.lbp.t1.l4 <- c(180.84, 182.31)
-sd.lbp.t1.l4 <- c(48.37, 48.41)
-f_range(m = m.lbp.t1.l4, sd = sd.lbp.t1.l4, n = n.lbp.t1, title="Weight")
-
-m.lbp.t1.l5 <- c(3.00, 3.28)
-sd.lbp.t1.l5 <- c(1.55, 1.29)
-f_range(m = m.lbp.t1.l5, sd = sd.lbp.t1.l5, n = n.lbp.t1, title="Group size")
-
-# Next line gives an F too small for rpsychi to calculate
-#m.lbp.t1.l6 <- c(6.62, 6.64)
-#sd.lbp.t1.l6 <- c(1.85, 2.06)
-#f_range(m = m.lbp.t1.l6, sd = sd.lbp.t1.l6, n = n.lbp.t1, title="Hungry then")
-
-m.lbp.t1.l7 <- c(1.88, 1.85)
-sd.lbp.t1.l7 <- c(1.34, 1.75)
-f_range(m = m.lbp.t1.l7, sd = sd.lbp.t1.l7, n = n.lbp.t1, title="Hungry now")
-
-cat("\n")
-cat("Lower Buffet Prices - Table 2\n")
-
-n.lbp.t2 <- c(62, 60)
-
-m.lbp.t2.l1 <- c(6.89, 7.44)
-sd.lbp.t2.l1 <- c(1.39, 1.60)
-f_range(m = m.lbp.t2.l1, sd = sd.lbp.t2.l1, n = n.lbp.t2, title="Overall taste")
-
-m.lbp.t2.l2 <- c(7.08, 7.45)
-sd.lbp.t2.l2 <- c(1.30, 1.60)
-f_range(m = m.lbp.t2.l2, sd = sd.lbp.t2.l2, n = n.lbp.t2, title="Piece 1, taste")
-
-m.lbp.t2.l3 <- c(7.08, 7.34)
-sd.lbp.t2.l3 <- c(1.37, 1.70)
-f_range(m = m.lbp.t2.l3, sd = sd.lbp.t2.l3, n = n.lbp.t2, title="Piece 1, satisfying")
-
-m.lbp.t2.l4 <- c(7.05, 7.47)
-sd.lbp.t2.l4 <- c(1.40, 1.55)
-f_range(m = m.lbp.t2.l4, sd = sd.lbp.t2.l4, n = n.lbp.t2, title="Piece 1, enjoyable")
-
-m.lbp.t2.l5 <- c(6.68, 7.97)
-sd.lbp.t2.l5 <- c(1.49, 1.21)
-f_range(m = m.lbp.t2.l5, sd = sd.lbp.t2.l5, n = n.lbp.t2, title="Piece 2, taste")
-
-m.lbp.t2.l6 <- c(6.68, 7.97)
-sd.lbp.t2.l6 <- c(1.49, 1.21)
-f_range(m = m.lbp.t2.l6, sd = sd.lbp.t2.l6, n = n.lbp.t2, title="Piece 2, satisfying")
-
-m.lbp.t2.l7 <- c(6.64, 7.81)
-sd.lbp.t2.l7 <- c(1.48, 1.22)
-f_range(m = m.lbp.t2.l7, sd = sd.lbp.t2.l7, n = n.lbp.t2, title="Piece 2, enjoyable")
-
-m.lbp.t2.l8 <- c(6.15, 7.58)
-sd.lbp.t2.l8 <- c(1.89, 1.39)
-f_range(m = m.lbp.t2.l8, sd = sd.lbp.t2.l8, n = n.lbp.t2, title="Piece 3, taste")
-
-m.lbp.t2.l9 <- c(6.16, 7.41)
-sd.lbp.t2.l9 <- c(1.87, 1.55)
-f_range(m = m.lbp.t2.l9, sd = sd.lbp.t2.l9, n = n.lbp.t2, title="Piece 3, satisfying")
-
-m.lbp.t2.l10 <- c(5.98, 7.45)
-sd.lbp.t2.l10 <- c(1.86, 1.52)
-f_range(m = m.lbp.t2.l10, sd = sd.lbp.t2.l10, n = n.lbp.t2, title="Piece 3, enjoyable")
